d2c92083743f38c430d0013463540bccaa70afb5
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-29 03:50:27 -0300

    feat: default mock preview on dev pages

--- mockups/manifest.js
+++ mockups/manifest.js
@@ -2,4 +2,6 @@ const MOCK_DATA_VERSION=1;
 const CRM_MOCK_PREFIX='valtren:mock:';
 const CRM_MOCK_RUNTIME_KEY=`${CRM_MOCK_PREFIX}runtime.v${MOCK_DATA_VERSION}`;
 const CRM_MOCK_BASE_DATE='2026-08-15';
+const CRM_MOCK_DEFAULT_PREVIEW_HOST='landersolucoestech-maker.github.io';
+const CRM_MOCK_DEFAULT_PREVIEW_PATH='/lander-solutions-site';
 const CRM_MOCK_STATE_KEYS=['crmCanonicalParties','crmDomain','crmRelContacts','crmRelLeads','crmAgendaEvents','crmFinancialTransactions','crmAccounting','crmFiscalDocuments','crmCostAllocations','crmLegalContracts','crmEconomicParticipations','crmPayouts','crmBusinessCatalog','crmMarketing','crmLegalMatters','crmCompliance','crmIntellectualProperty','crmCorporateGovernance','crmNotifications'];
